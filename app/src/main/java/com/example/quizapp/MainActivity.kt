662aa518bad33c3f31fcb8cc97e55bcd2bbdb89e
Nick <nikoloz.kokhreidze.1@btu.edu.ge>
2022-12-07 22:11:21 +0400

    final commit

---
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.quizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnstart : Button = findViewById(R.id.btn_start)
+        val etname : EditText = findViewById(R.id.et_name)
+
+        btnstart.setOnClickListener {
+            if(etname.text.isEmpty()){
+                Toast.makeText(this, "please enter your name", Toast.LENGTH_LONG).show()
+
+            }else{
+                val intent = Intent(this,QuizQuestionsActivity::class.java)
+                intent.putExtra(Constants.USER_NAME,etname.text.toString())
+                startActivity(intent)
+                finish()
+            }
+        }
+
+
+    }
+}
